67270ce1b286a663cff323f86c7129ab49a300ed
kaiiboraka <kaiiboraka@gmail.com>
2022-07-06 03:44:23 -0600

    simplified main even further lol

---
+++ Datalog.cpp
@@ -0,0 +1,48 @@
+//
+// Created by Brandon on 7/6/2022.
+//
+
+#include "Datalog.h"
+
+vector<Token> LexTokens(const string& input)
+{
+	Lexer lexer;
+	return lexer.Run(input);
+}
+
+string TryReadFile(const string& path)
+{
+	try
+	{
+		return Helper::ReadFileIntoString(path);
+	}
+	catch (exception& e)
+	{
+		cerr << e.what() << endl;
+		return NULL;
+	}
+}
+
+string TryReadArgs(const string& argv)
+{
+	ifstream ifs(argv);
+	if (!ifs.is_open()) ifs.open(ARGS_PATH + argv);
+	string line, output;
+	while (getline(ifs, line)) output += line + '\n';
+	return output;
+}
+
+void TryParse(const vector<Token>& tokens, ostream& os)
+{
+	try
+	{
+		Parser parser = Parser(tokens);
+		parser.Run();
+		os << "Success!" << endl;
+	}
+	catch (Token& errorToken)
+	{
+		os << "Failure!" << endl << "  " << errorToken.ToString() << endl;
+	}
+}
+
---
+++ Datalog.h
@@ -0,0 +1,32 @@
+//
+// Created by Brandon on 7/6/2022.
+//
+
+#ifndef PARSER_H_DATALOG_H
+#define PARSER_H_DATALOG_H
+
+#include "Lexer.h"
+#include "Parser.h"
+#include <fstream>
+
+const string ROOT_PATH = "../";
+const string ARGS_PATH = "Project2aPassOffCases/2-80/";
+const string FILE_PATH = ROOT_PATH + "input.txt";
+
+void TryParse(const vector<Token>& tokens, ostream& os);
+
+string TryReadFile(const string& path);
+
+string TryReadArgs(const string& argv);
+
+vector<Token> LexTokens(const string& input);
+
+class Datalog
+{
+	Lexer lexer;
+	Parser parser;
+
+};
+
+
+#endif //PARSER_H_DATALOG_H
